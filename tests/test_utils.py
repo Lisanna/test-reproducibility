7ff58ef9b4e562f139da01b7cd381041e34c6e92
paladin <lisanna.paladin@embl.de>
2021-07-29 16:20:42 +0200

    add unittest

---
+++ tests/test_utils.py
@@ -0,0 +1,22 @@
+"""Test the utility functions."""
+
+import unittest
+from src.template_package.utils import iter_together
+import pathlib
+
+HERE = pathlib.Path(__file__).parent.resolve()
+
+
+class TestUtils(unittest.TestCase):
+    """Test the utilities."""
+
+    def test_my_function(self):
+        """Test the :func: iter_together."""
+        path_1 = HERE.joinpath('test1.txt')
+        path_2 = HERE.joinpath('test2.txt')
+        results = iter_together(path_1, path_2)
+        expected = [
+            ('hello', 'goodbye'),
+            ('ciao', 'ciao')
+        ]
+        self.assertEqual(expected, results)
